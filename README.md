4baa238a493e24f6ffa3b28ae505d42eff9f1b78
daniel Laura <davieltk@gmail.com>
2021-07-07 16:25:45 -0400

    figuras geometricas parte 1

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # platzi-curso-practico-javascript
+...
+
+## Taller #1: figuras geométricas
+
+- Primer paso: definir las fórmulas
+- segundo paso: implementar las fórmulas  en Javascript
+- Tercer paso: crear funciones
+- Cuarto paso: integrar JS con HTML
---
+++ figuras.js
@@ -0,0 +1,28 @@
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden:" + ladoCuadrado +" cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perimetro del cuadrado es:" + perimetroCuadrado+" cm");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("El area del cuadrado es:" + areaCuadrado+" cm^2");
+
+
+//Codigo del triangulo
+
+const ladotriangulo1 = 6;
+
+const ladotriangulo2 = 6;
+const baseTriangulo = 4;
+
+console.log("Los lados del triangulo miden:" 
++ ladotriangulo1 
++" cm, "
++ ladotriangulo2 
++"cm ," 
++ baseTriangulo 
++ "cm"
+);
+
+
+
